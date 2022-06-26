0e81d72461e56a778d84bee078c6282b4cbf7869
DemidoovMax <uslugimoi@gmail.com>
2022-06-26 16:01:01 +0300

    Init

--- Mars.java
+++ Mars.java
@@ -1,5 +1,6 @@
 public class Mars {
     public static void main(String[] args) {
         System.out.println("Privet");
+        System.out.println("Hello World");
     }
 }
---
+++ Mars2.java
@@ -0,0 +1,5 @@
+public class Mars2 {
+    public static void main(String[] args) {
+        System.out.println(1289723899);
+    }
+}
