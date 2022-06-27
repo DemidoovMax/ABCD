79427ff40e8b3012432dba13d3b744bc07c64f2b
DemidoovMax <uslugimoi@gmail.com>
2022-06-27 16:17:17 +0300

    Init2

--- Mars2.java
+++ Mars2.java
@@ -2,5 +2,6 @@ public class Mars2 {
     public static void main(String[] args) {
         System.out.println(1289723899);
         System.out.println(00000);
+        System.out.println(0000);
     }
 }
