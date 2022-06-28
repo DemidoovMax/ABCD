cee73901a94f1f827f448aeec22e308056f6d08c
DemidoovMax <uslugimoi@gmail.com>
2022-06-28 17:23:10 +0300

    Добавили цифру 1

--- Mars.java
+++ Mars.java
@@ -1,6 +1,6 @@
 public class Mars {
     public static void main(String[] args) {
         System.out.println("Privet");
-        System.out.println("Hello World");
+        System.out.println("Hello World1");
     }
 }
